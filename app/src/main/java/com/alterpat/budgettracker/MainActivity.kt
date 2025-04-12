57a8a6dc6d09d39a3b3543734b0e6d635e5e3d11
Vaceslav676 <vaceslavzubenko676@gmail.com>
2025-04-12 09:37:16 +0300

    Initial commit

---
+++ app/src/main/java/com/alterpat/budgettracker/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.alterpat.budgettracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_add_transaction.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var deletedTransaction: Transaction
+    private lateinit var transactions: MutableList<Transaction> // Изменено на MutableList
+    private lateinit var oldTransactions: List<Transaction>
+    private lateinit var transactionAdapter: TransactionAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var db: AppDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        transactions = arrayListOf()
+
+        transactionAdapter = TransactionAdapter(transactions)
+        linearLayoutManager = LinearLayoutManager(this)
+
+        db = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java,
+            "transactions"
+        ).build()
+
+        recyclerview.apply {
+            adapter = transactionAdapter
+            layoutManager = linearLayoutManager
+        }
+
+        // Swipe to remove
+        val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN, // Включаем drag-and-drop
+            ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // Получаем позиции текущего элемента и целевого
+                val fromPosition = viewHolder.adapterPosition
+                val toPosition = target.adapterPosition
+
+                // Меняем элементы местами в списке
+                if (fromPosition < toPosition) {
+                    for (i in fromPosition until toPosition) {
+                        transactions[i] = transactions[i + 1].also { transactions[i + 1] = transactions[i] }
+                    }
+                } else {
+                    for (i in fromPosition downTo toPosition + 1) {
+                        transactions[i] = transactions[i - 1].also { transactions[i - 1] = transactions[i] }
+                    }
+                }
+
+                // Уведомляем адаптер о перемещении
+                transactionAdapter.notifyItemMoved(fromPosition, toPosition)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                deleteTransaction(transactions[viewHolder.adapterPosition])
+            }
+        }
+
+        val swipeHelper = ItemTouchHelper(itemTouchHelper)
+        swipeHelper.attachToRecyclerView(recyclerview)
+
+        addBtn.setOnClickListener {
+            val intent = Intent(this, AddTransactionActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun fetchAll() {
+        GlobalScope.launch {
+            transactions = db.transactionDao().getAll().toMutableList() // Преобразуем в MutableList
+
+            runOnUiThread {
+                updateDashboard()
+                transactionAdapter.setData(transactions)
+            }
+        }
+    }
+
+    private fun updateDashboard() {
+        val totalAmount = transactions.map { it.amount }.sum()
+        val budgetAmount = transactions.filter { it.amount > 0 }.map { it.amount }.sum()
+        val expenseAmount = totalAmount - budgetAmount
+
+        balance.text = "$ %.2f".format(totalAmount)
+        budget.text = "$ %.2f".format(budgetAmount)
+        expense.text = "$ %.2f".format(expenseAmount)
+    }
+
+    private fun undoDelete() {
+        GlobalScope.launch {
+            db.transactionDao().insertAll(deletedTransaction)
+
+            transactions = oldTransactions.toMutableList() // Преобразуем в MutableList
+
+            runOnUiThread {
+                transactionAdapter.setData(transactions)
+                updateDashboard()
+            }
+        }
+    }
+
+    private fun showSnackbar() {
+        val view = findViewById<View>(R.id.coordinator)
+        val snackbar = Snackbar.make(view, "Transaction deleted!", Snackbar.LENGTH_LONG)
+        snackbar.setAction("Undo") {
+            undoDelete()
+        }
+            .setActionTextColor(ContextCompat.getColor(this, R.color.red))
+            .setTextColor(ContextCompat.getColor(this, R.color.white))
+            .show()
+    }
+
+    private fun deleteTransaction(transaction: Transaction) {
+        deletedTransaction = transaction
+        oldTransactions = transactions.toList() // Создаем копию списка
+
+        GlobalScope.launch {
+            db.transactionDao().delete(transaction)
+
+            transactions = transactions.filter { it.id != transaction.id }.toMutableList() // Преобразуем в MutableList
+
+            runOnUiThread {
+                updateDashboard()
+                transactionAdapter.setData(transactions)
+                showSnackbar()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fetchAll()
+    }
+}
